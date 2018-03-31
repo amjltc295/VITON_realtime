26788460ae72dc83049a1c83f755aad813b41cb5
jasonoscar88 <jasonoscar88@gmail.com>
2018-04-01 07:57:44 +0800

    read images of trimmed clothes

--- flat_attach_clothes.py
+++ flat_attach_clothes.py
@@ -1,18 +1,15 @@
-# import pickle
+import pickle
 import cv2
 import numpy as np
 import time
-
 
 def flat_attach_clothes(mask, original, clothes, debug=False):
     '''
     # mask: pickle file
     mask = pickle.load(open('outputs/20180331_08431522457007_mask.pickle',
                             'rb'))
-
     # original: original person img w. background
     original = cv2.imread('outputs/original.jpg')
-
     # clothes: clothes image with white background
     clothes = cv2.imread('outputs/clothes.jpg')
     '''
@@ -47,13 +44,49 @@ def flat_attach_clothes(mask, original, clothes, debug=False):
     print(time.time() - t)
     t = time.time()
 
+    # new_clothes = trim_clothes(clothes)
+    new_clothes = cv2.imread('outputs/save/clothes_trim.jpg')
+    print(time.time() - t)
+    t = time.time()
+    
+    # resize clothes to size of segmentation
+    resized_image = cv2.resize(new_clothes, (right-left, bot-top))
+    if (debug):
+        cv2.imwrite('outputs/save/clothes_resize.jpg', resized_image)
+
+    final = clothes_in_segmentation(resized_image, mymask, original, (top, left))
+    print(time.time() - t)
+    if (debug):
+        cv2.imwrite('outputs/save/new_pic.jpg', original)
+    return original
+
+
+def clothes_in_segmentation(clothes, mask, original, original_start_edge, debug = False):
+    # get clothes in segmentation shape
+    (top, left) = original_start_edge
+
+    cloth_rev = cv2.bitwise_not(clothes)
+    filt = cv2.bitwise_and(cloth_rev, mask)
+    filt = cv2.bitwise_not(filt)
+    if (debug): 
+        cv2.imwrite('outputs/save/tmp.jpg', filt)
+
+    for i in range(filt.shape[0]):
+        for j in range(filt.shape[1]):
+            if sum(filt[i, j]) != 255*3:
+                original[top+i, left+j] = filt[i, j]
+
+    return original
+
+def trim_clothes(clothes, debug = False):
     # trim white sides of target clothes
     row, col, _ = clothes.shape
     tt, ll = 99999, 99999
     bb, rr = -1, -1
     for i in range(0, row, 2):
         for j in range(0, col, 2):
-            if (clothes[i][j][0] == 255 and
+            if not (clothes[i][j][0] == 255 and
+                    clothes[i][j][1] == 255 and
                     clothes[i][j][2] == 255):
                 if i < tt:
                     tt = i
@@ -64,56 +97,16 @@ def flat_attach_clothes(mask, original, clothes, debug=False):
                 if j > rr:
                     rr = j
 
-    print(time.time() - t)
-    t = time.time()
     # trim sleeves
     wid, hei = bb-tt, rr-ll
     new_clothes = clothes[tt+wid//20:bb-wid//20, ll+hei//6:rr-hei//6]
     if (debug):
         cv2.imwrite('outputs/save/clothes_trim.jpg', new_clothes)
+    return new_clothes
 
-    # resize clothes to size of segmentation
-    resized_image = cv2.resize(new_clothes, (right-left, bot-top))
-    if (debug):
-        cv2.imwrite('outputs/save/clothes_resize.jpg', resized_image)
+if __name__ == '__main__':
+    mask = pickle.load(open('outputs/20180331_08431522457007_mask.pickle', 'rb'))
+    original = cv2.imread('outputs/original.jpg')
+    clothes = cv2.imread('outputs/clothes.jpg')
 
-    # get clothes in segmentation shape
-    cloth_rev = cv2.bitwise_not(resized_image)
-    filt = cv2.bitwise_and(cloth_rev, mymask)
-    filt = cv2.bitwise_not(filt)
-    if (debug):
-        cv2.imwrite('outputs/save/tmp.jpg', filt)
-    print(time.time() - t)
-    t = time.time()
-
-    filt_w = filt.shape[0]
-    filt_h = filt.shape[1]
-    for i in range(filt_w):
-        for j in range(filt_h):
-            if sum(filt[i, j]) != 255*3:
-                original[top+i, left+j] = filt[i, j]
-
-    print(time.time() - t)
-    if (debug):
-        cv2.imwrite('outputs/save/new_pic.jpg', original)
-    return original
-
-# # sample code
-# Now create a mask of logo and create its inverse mask also
-# img2gray = cv2.cvtColor(clothes,cv2.COLOR_BGR2GRAY)
-# ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
-# print(type(ret))
-# print(type(mask))
-# print(mask[100])
-# input()
-# mask_inv = cv2.bitwise_not(mask)
-# # Now black-out the area of logo in ROI
-# img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
-# # Take only region of logo from logo image.
-# img2_fg = cv2.bitwise_and(clothes,clothes,mask = mask)
-# # Put logo in ROI and modify the main image
-# dst = cv2.add(img1_bg,img2_fg)
-# img1[0:rows, 0:cols ] = dst
-# cv2.imshow('res',img1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    flat_attach_clothes(mask, original, clothes, debug = True)
